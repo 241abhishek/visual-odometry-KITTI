19462c77c0fc10e1bb64006dca41f0ac4cb87219
Abhishek <241abhishek@gmail.com>
2024-07-02 15:35:51 -0500

    Created function to decompose the projection matrix

--- frames.py
+++ frames.py
@@ -5,6 +5,7 @@ import cv2
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 def compute_left_disparity_map(img_left, img_right, verbose=False):
     """
@@ -31,15 +32,68 @@ def compute_left_disparity_map(img_left, img_right, verbose=False):
 
     return disparity
 
+def decompose_projection_matrix(p):
+    """
+    Decompose the projection matrix into intrinsic and extrinsic parameters.
+
+    Args:
+        p (numpy.ndarray): the projection matrix.
+
+    Returns:
+        k (numpy.ndarray): the intrinsic matrix.
+        r (numpy.ndarray): the rotation matrix.
+        t (numpy.ndarray): the translation vector. 
+    """
+    
+    # decompose the projection matrix
+    k, r, t, _, _, _, _ = cv2.decomposeProjectionMatrix(p)
+    t = (t / t[3])[:3]
+
+    return k, r, t
+
+
+def main(test_function):
+    """
+    Main function to test the utility functions.
+
+    Args:
+        test_function (str): the function to test.
+    """
+
+    if test_function == 'compute_left_disparity_map':
+        # test the compute_left_disparity_map function
+        img_left = cv2.imread('../dataset/sequences/00/image_0/000000.png', cv2.IMREAD_GRAYSCALE)
+        img_right = cv2.imread('../dataset/sequences/00/image_1/000000.png', cv2.IMREAD_GRAYSCALE)
+        disparity = compute_left_disparity_map(img_left, img_right, verbose=True)
+        plt.figure(figsize=(10, 10))
+        plt.imshow(disparity)
+        plt.title('Disparity map')
+        plt.show(block=False)
+        plt.pause(5)
+        plt.close()
+    
+    if test_function == 'decompose_projection_matrix':
+        # test the decompose_projection_matrix function
+        p = np.array([[7.215377e+02, 0.000000e+00, 6.095593e+02, 4.485728e+01],
+                      [0.000000e+00, 7.215377e+02, 1.728540e+02, 2.163791e-01],
+                      [0.000000e+00, 0.000000e+00, 1.000000e+00, 2.745884e-03]])
+        k, r, t = decompose_projection_matrix(p)
+        print('Intrinsic matrix:')
+        print(k)
+        print('Rotation matrix:')
+        print(r)
+        print('Translation vector:')
+        print(t)
+
 if __name__ == '__main__':
-    # test the compute_left_disparity_map function
-    img_left = cv2.imread('../dataset/sequences/00/image_0/000000.png', cv2.IMREAD_GRAYSCALE)
-    img_right = cv2.imread('../dataset/sequences/00/image_1/000000.png', cv2.IMREAD_GRAYSCALE)
-    disparity = compute_left_disparity_map(img_left, img_right, verbose=True)
-    plt.figure(figsize=(10, 10))
-    plt.imshow(disparity)
-    plt.title('Disparity map')
-    plt.show(block=False)
-    plt.pause(5)
-    plt.close()
+
+    parser = argparse.ArgumentParser(description='Utility functions to manipulate image frames.')
+    parser.add_argument('--test_function', type=str,
+                        default='compute_left_disparity_map',
+                        choices=['compute_left_disparity_map', 'decompose_projection_matrix'], 
+                        help='The function to test.')
+
+    args = parser.parse_args()
+
+    main(args.test_function)
 
