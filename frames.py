1a5be3aee694d4c2a1707a45c09d2d00434ef6ad
Abhishek <241abhishek@gmail.com>
2024-07-02 15:55:55 -0500

    Added a function to calculate the depth map

--- frames.py
+++ frames.py
@@ -21,7 +21,9 @@ def compute_left_disparity_map(img_left, img_right, verbose=False):
     """
 
     # compute the disparity map
-    matcher = cv2.StereoBM_create(numDisparities=96, blockSize=11)
+    matcher = cv2.StereoSGBM_create(numDisparities=96, minDisparity=0, blockSize=11,
+                                    P1 = 8*3*6**2, P2 = 32*3*6**2,
+                                    mode = cv2.STEREO_SGBM_MODE_SGBM_3WAY)
 
     start = datetime.datetime.now()
     disparity = matcher.compute(img_left, img_right).astype(np.float32) / 16.0
@@ -50,6 +52,36 @@ def decompose_projection_matrix(p):
     t = (t / t[3])[:3]
 
     return k, r, t
+
+def calc_depth_map(disp_left, k_left, t_left, t_right):
+    """
+    Calculate the depth map from the disparity map.
+
+    Args:
+        disp_left (numpy.ndarray): the disparity map.
+        k_left (numpy.ndarray): the intrinsic matrix of the left camera.
+        t_left (numpy.ndarray): the translation vector of the left camera.
+        t_right (numpy.ndarray): the translation vector of the right camera.
+
+    Returns:
+        numpy.ndarray: the depth map.
+    """
+
+    # get the focal length of the x axis for the left camera
+    f = k_left[0, 0]
+
+    # calculate the baseline
+    b = t_right[0] - t_left[0]
+
+    # avoid instability and division by zero
+    disp_left[disp_left == 0.0] = 0.1
+    disp_left[disp_left == -1.0] = 0.1
+
+    # calculate the depth map
+    depth_map = np.ones(disp_left.shape)
+    depth_map = f * b / disp_left
+
+    return depth_map
 
 
 def main(test_function):
@@ -85,12 +117,27 @@ def main(test_function):
         print('Translation vector:')
         print(t)
 
+    if test_function == 'calc_depth_map':
+        from data import Dataset_Handler
+        # test the calc_depth_map function
+        dataset = Dataset_Handler('00')
+        disp_left = compute_left_disparity_map(dataset.first_image_left, dataset.first_image_right)
+        k_left, r_left, t_left = decompose_projection_matrix(dataset.P0)
+        k_right, r_right, t_right = decompose_projection_matrix(dataset.P1)
+        depth_map = calc_depth_map(disp_left, k_left, t_left, t_right)
+        plt.figure(figsize=(10, 10))
+        plt.imshow(depth_map)
+        plt.title('Depth map')
+        plt.show(block=False)
+        plt.pause(5)
+        plt.close()
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Utility functions to manipulate image frames.')
     parser.add_argument('--test_function', type=str,
                         default='compute_left_disparity_map',
-                        choices=['compute_left_disparity_map', 'decompose_projection_matrix'], 
+                        choices=['compute_left_disparity_map', 'decompose_projection_matrix', 'calc_depth_map'], 
                         help='The function to test.')
 
     args = parser.parse_args()
