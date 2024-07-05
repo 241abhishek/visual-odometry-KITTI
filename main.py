cb75122f5d8d5532fef706bfcb5f97c9477adf6a
Abhishek <241abhishek@gmail.com>
2024-07-05 13:41:38 -0500

    Added ability to load different sequences using the command line

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ and visualize the results
 from data import Dataset_Handler
 from frames import visual_odometry
 from utils import visualize_3d_path
+from argparse import ArgumentParser
 
-def main():
+def main(*args):
+
+    args = ArgumentParser()
+    args.add_argument('--sequence', type=str, default='00', help='KITTI sequence number - from 00 to 10')
+    args = args.parse_args()
+
     # load the dataset
-    dataset = Dataset_Handler('00')
+    seq = args.sequence
+    dataset = Dataset_Handler(seq)
     print('Number of frames:', dataset.num_frames)
     print('Image dimensions:', dataset.img_height, dataset.img_width)
     
